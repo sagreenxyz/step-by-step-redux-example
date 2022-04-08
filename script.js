bf0efa476325a79fb861462395ada64c1eacf854
SAGreenXYZ <sagreenxyz@gmail.com>
2022-04-07 20:54:04 -0500

    4. adding element variables in javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const counterValue = document.getElementById('counter-value')
+const actionType = document.getElementById('action-type')
+const btnPlus = document.getElementById('btn-plus')
+const btnMinus = document.getElementById('btn-minus')
+
+console.log(counterValue.innerText)
+console.log(actionType.innerText)
+console.log(btnPlus.innerText)
+console.log(btnMinus.innerText)

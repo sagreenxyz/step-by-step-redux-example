ec0e6da3600d7286db11133ca1a83aab486b0d47
SAGreenXYZ <sagreenxyz@gmail.com>
2022-04-07 21:28:10 -0500

    20. adding functionality to increment/decrement by 5; note simplicity of adding new functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const counterValue = document.getElementById('counter-value')
 const actionType = document.getElementById('action-type')
 const btnPlus = document.getElementById('btn-plus')
 const btnMinus = document.getElementById('btn-minus')
+const btnPlus5 = document.getElementById('btn-plus5')
+const btnMinus5 = document.getElementById('btn-minus5')
 
 const initialState = {
     counterValue: 0,
@@ -14,6 +16,10 @@ let store = Redux.createStore((state = initialState, action) => {
             return { counterValue: state.counterValue + 1, actionType: 'counter/incremented' }
         case 'counter/decremented':
             return { counterValue: state.counterValue - 1, actionType: 'counter/decremented' }
+        case 'counter/incremented5':
+            return { counterValue: state.counterValue + 5, actionType: 'counter/incremented5' }
+        case 'counter/decremented5':
+            return { counterValue: state.counterValue - 5, actionType: 'counter/decremented5' }
         default:
             return state
     }
@@ -25,6 +31,14 @@ btnPlus.addEventListener('click', () => {
 
 btnMinus.addEventListener('click', () => {
     store.dispatch({ type: 'counter/decremented' })
+})
+
+btnPlus5.addEventListener('click', () => {
+    store.dispatch({ type: 'counter/incremented5' })
+})
+
+btnMinus5.addEventListener('click', () => {
+    store.dispatch({ type: 'counter/decremented5' })
 })
 
 const handleStateChangeResults = () => {
